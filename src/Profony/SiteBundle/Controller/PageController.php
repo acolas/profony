8975ff4da2ec9c08bae439865e9e2adae4d46b86
Anthony Colas <anthony.colas@gmail.com>
2012-01-08 23:27:06 +0100

    add contact page

--- src/Profony/SiteBundle/Controller/PageController.php
+++ src/Profony/SiteBundle/Controller/PageController.php
@@ -1,17 +1,51 @@
 <?php
+
 namespace Profony\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// Import new namespaces
+use Profony\SiteBundle\Entity\Enquiry;
+use Profony\SiteBundle\Form\EnquiryType;
 
-class PageController extends Controller
-{
-    public function indexAction()
-    {
+class PageController extends Controller {
+
+    public function indexAction() {
         return $this->render('ProfonySiteBundle:Page:index.html.twig');
     }
-    
-    public function aboutAction()
-    {
+
+    public function aboutAction() {
         return $this->render('ProfonySiteBundle:Page:about.html.twig');
     }
+
+    public function contactAction() {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+
+                $message = \Swift_Message::newInstance()
+                        ->setSubject('Contact enquiry from symblog')
+                        ->setFrom('su@per.fr')
+                        ->setTo($this->container->getParameter('profony_site.emails.contact_email'))
+                        ->setBody($this->renderView('ProfonySiteBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->setFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+                return $this->redirect($this->generateUrl('ProfonySiteBundle_contact'));
+            }
+        }
+
+        return $this->render('ProfonySiteBundle:Page:contact.html.twig', array(
+                    'form' => $form->createView()
+                ));
+    }
+
 }
