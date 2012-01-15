32e774be9cccc70804ae19341062d171643636d5
Anthony Colas <anthony.colas@gmail.com>
2012-01-15 20:11:33 +0100

    add doctrine model and fake data

---
+++ src/Profony/SiteBundle/Controller/BlogController.php
@@ -0,0 +1,28 @@
+<?php
+namespace Profony\SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Blog controller.
+ */
+class BlogController extends Controller
+{
+    /**
+     * Show a blog entry
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $blog = $em->getRepository('ProfonySiteBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $this->render('ProfonySiteBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+        ));
+    }
+}
